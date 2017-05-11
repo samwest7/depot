96f7c799f6a0f959b02654bf3c86a743ded002e3
Sam Westfield <sam.westfield@introhive.com>
2017-05-11 15:24:59 -0300

    Version 14.1 After Crash Fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 require 'digest/sha2'
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :hashed_password, :salt
+  attr_accessible :name, :password_confirmation, :password
 
   validates :name, :presence => true, :uniqueness => true
   attr_accessor :password_confirmation
-  attr_reader :password
+  #attr_reader :password
 
   validate :password_must_be_present
 
   def User.authenticate(name, password)
     if user = find_by_name(name)
       if user.hashed_password == encrypt_password(password, user.salt)
+        user
       end
     end
   end
@@ -25,6 +26,7 @@ class User < ActiveRecord::Base
   def password=(password)
     @password = password
     if password.present?
+      generate_salt
       self.hashed_password = self.class.encrypt_password(password, salt)
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Depot::Application.routes.draw do
-  get 'admin' => 'admin#index'
+  #get 'admin' => 'admin#index'
 
-  controller :sessions do
-    get 'login' => :new
-    post 'login' => :create
-    delete 'logout' => :destroy
-  end
+  #controller :sessions do
+  #  get 'login' => :new
+  #  post 'login' => :create
+  #  delete 'logout' => :destroy
+  #end
 
 
 
